ec49e46351d093ae54a9b34b4a299da9ff1b8218
Maddy O'Brien Jones <35735580+maddyobrienjones@users.noreply.github.com>
2020-10-16 13:25:34 -0400

    working on accessing search results

--- takehome/takehome/spiders/crawler.py
+++ takehome/takehome/spiders/crawler.py
@@ -1,10 +1,24 @@
 import scrapy
-
+from selenium import webdriver
 
 class CrawlerSpider(scrapy.Spider):
     name = 'crawler'
     allowed_domains = ['https://firststop.sos.nd.gov/search/business']
-    start_urls = ['http://https://firststop.sos.nd.gov/search/business/']
+    start_urls = ['https://firststop.sos.nd.gov/search/business/']
 
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        
     def parse(self, response):
+        self.driver.get(response.url)
+        searchInput = self.driver.find_element_by_class_name("search-input")
+        searchInput.send_keys("X")
+        advSearch = self.driver.find_element_by_class_name("advanced-search-toggle")
+        advSearch.click()
+        activeOnly = self.driver.find_element_by_xpath("//*[@id='field-ACTIVE_ONLY_YN']")
+        activeOnly.click()
+        searchButton = self.driver.find_element_by_class_name("btn btn-primary btn-raised advanced-search-button")
+        searchButton.click()
         pass
+
+
