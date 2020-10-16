a31af52aedd2d7170d1042a69e250eaeac86423c
Maddy O'Brien Jones <35735580+maddyobrienjones@users.noreply.github.com>
2020-10-16 15:34:58 -0400

    probably will end up not using this code
    after working on this, went back to the site and looked at network activity. as the task said it could be done in simple scrapy, i figured i was overcomplicating things. realized that instead of this i should probably work on figuring out the identifiers used in the api and just replicate the api requests that the site makes

--- takehome/takehome/spiders/crawler.py
+++ takehome/takehome/spiders/crawler.py
@@ -1,5 +1,9 @@
 import scrapy
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 class CrawlerSpider(scrapy.Spider):
     name = 'crawler'
@@ -8,17 +12,18 @@ class CrawlerSpider(scrapy.Spider):
 
     def __init__(self):
         self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
         
     def parse(self, response):
         self.driver.get(response.url)
-        searchInput = self.driver.find_element_by_class_name("search-input")
-        searchInput.send_keys("X")
-        advSearch = self.driver.find_element_by_class_name("advanced-search-toggle")
-        advSearch.click()
+        self.driver.find_element_by_class_name("search-input").send_keys("X")
+        #searchButton = self.driver.find_element_by_xpath('//*[@id="root"]/div/div[3]/div/main/div/div/button')
+        #searchButton.click()
+        self.driver.find_element_by_class_name("advanced-search-toggle").click()
         activeOnly = self.driver.find_element_by_xpath("//*[@id='field-ACTIVE_ONLY_YN']")
         activeOnly.click()
         searchButton = self.driver.find_element_by_class_name("btn btn-primary btn-raised advanced-search-button")
-        searchButton.click()
+        table = self.driver.find_element_by_class_name("div-table center-container")
         pass
 
 
